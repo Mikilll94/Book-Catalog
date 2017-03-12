7e8803d0cb6545b9d7173041aa2da9a974d5d575
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2017-03-12 22:28:24 +0100

    WPF - laborki

---
+++ Book Catalog/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using HomeLibrary;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Book_Catalog
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Book> booksList = MyBookCollection.GetMyCollection();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            BooksList.ItemsSource = booksList;
+            BookFormats.ItemsSource = Enum.GetValues(typeof(BookFormat));
+        }
+
+        private void AddBook_Click(object sender, RoutedEventArgs e)
+        {
+            booksList.Add(new Book(1)
+            {
+                Author = Author_txtbox.Text,
+                Format = (BookFormat)BookFormats.SelectedItem,
+                IsRead = (bool)IsRead_checkbox.IsChecked,
+                Title = Title_txtbox.Text,
+                Year = int.Parse(Year_txtbox.Text)
+            });
+        }
+
+        private void EditBook_Click(object sender, RoutedEventArgs e)
+        {
+            Author_txtbox.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            BookFormats.GetBindingExpression(ComboBox.SelectedItemProperty).UpdateSource();
+            IsRead_checkbox.GetBindingExpression(CheckBox.IsCheckedProperty).UpdateSource();
+            Title_txtbox.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            Year_txtbox.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+        }
+
+        private void DeleteBook_Click(object sender, RoutedEventArgs e)
+        {
+            booksList.RemoveAt(BooksList.SelectedIndex);
+        }
+    }
+}
